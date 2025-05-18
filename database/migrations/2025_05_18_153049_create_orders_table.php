10de62b2cce81b2b6cec27f234392f540f823917
Valiant <c14220157@john.petra.ac.id>
2025-05-18 22:56:27 +0700

    generate msc

---
+++ database/migrations/2025_05_18_153049_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->double('total_price');
+            $table->string('payment_status')->nullable();
+            $table->dateTime('payment_date')->nullable();
+            $table->string('snap_token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
